9bfa7b6a6964bf222ecf1d0b5a3333e372fb67bc
AMKare <annakareeva@gmail.com>
2018-06-04 10:24:09 -0400

    Update
    I saved damnit.

--- src/Creatures.java
+++ src/Creatures.java
@@ -9,7 +9,31 @@ public class Creatures extends Card
 		
 	}
 
-		
+	public int getAtk() {
+		return atk;
+	}
+
+	public void setAtk(int atk) {
+		this.atk = atk;
+	}
+
+	public int getDef() {
+		return def;
+	}
+
+	public void setDef(int def) {
+		this.def = def;
+	}
+
+	public int getCost() {
+		return cost;
+	}
+
+	public void setCost(int cost) {
+		this.cost = cost;
+	}
+
+	
 	
 
 }
--- src/Spell.java
+++ src/Spell.java
@@ -1,11 +1,17 @@
 
 public class Spell extends Card
 {
-
-	public Spell (String Name) {
+	private int cost;
+	public Spell (String Name, int cost) {
 		
 		super(Name);
 		
+	}
+	public int getCost() {
+		return cost;
+	}
+	public void setCost(int cost) {
+		this.cost = cost;
 	}
 	
 }
