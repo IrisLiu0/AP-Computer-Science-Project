71563c306d855ab3acaeb5447fcce6c0bc6ea2ab
Joebob9754 <bdeatz@gmail.com>
2018-06-06 09:28:11 -0400

    edits

--- src/GraphicsEngine.java
+++ src/GraphicsEngine.java
@@ -2,21 +2,23 @@ import java.awt.Component;
 import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
 
 import javax.swing.Timer;
 
 
-public class GraphicsEngine extends Component implements ActionListener
+public class GraphicsEngine extends Component implements ActionListener, MouseListener, MouseMotionListener
 {
 	private Timer t;
-	private Card[] hand = new Card[5];
+	private Player p;
 	
 	public GraphicsEngine()
 	{
-		for (int i = 0; i < hand.length; i++)
-		{
-			hand[i] = new Card("Card "+i);
-		}
+		this.addMouseListener(this);
+		this.addMouseMotionListener(this);
+		p = new Player();
 		t = new Timer(10, this);
 		t.start();
 	}
@@ -29,15 +31,47 @@ public class GraphicsEngine extends Component implements ActionListener
 	public void paint(Graphics g)
 	{
 		//g.fillRect(0, 0, this.getWidth(), this.getHeight());
-		for (int i = 0; i < hand.length; i++)
-		{
-			hand[i].paint(g, 200+i*110, 800, 100, 120);
-		}
-
+		p.drawHand(g);
+		
 	}
 
 	public void actionPerformed(ActionEvent e) 
 	{
 		update();
+	}
+
+	public void mouseDragged(MouseEvent e)
+	{
+
+	}
+
+	public void mouseMoved(MouseEvent e)
+	{
+
+	}
+
+	public void mouseClicked(MouseEvent e)
+	{
+
+	}
+
+	public void mouseEntered(MouseEvent e)
+	{
+
+	}
+
+	public void mouseExited(MouseEvent e)
+	{
+
+	}
+
+	public void mousePressed(MouseEvent e)
+	{
+
+	}
+
+	public void mouseReleased(MouseEvent e)
+	{
+
 	}
 }
