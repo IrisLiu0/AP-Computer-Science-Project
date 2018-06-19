350f53ec40f2129e457e53472a6f7adefd92526b
Iris <>
2018-06-18 21:51:28 -0400

    added a attack thing to enemy

--- src/Enemy.java
+++ src/Enemy.java
@@ -13,21 +13,19 @@ public class Enemy {
 				new Minion("Pleb", 0, 10, 1, 2), new Minion("Pleb", 0, 10, 1, 2), new Minion("Pleb", 0, 10, 1, 2)));
 	}
 
-	public void startGame() {
-
-	}
-
-	public void startTurn() {
-		if (Math.random() * 2 > 0)
-			;
+	public void attack(Player p) {
+		for (Minion m : field) {
+			p.changeLife(1);
+		}
 	}
 
 	public void paint(Graphics g, int width) {
-		g.setFont(new Font("ARIAL", 50,50));
+		g.setFont(new Font("ARIAL", 50, 50));
 		for (int i = 0; i < field.size(); i++) {
 			field.get(i).paint(g, width / 2 - (width / 20) * (field.size()) + width / 10 * i, 125, width / 10,
 					(int) (width / 10 / Card.aspectRatio));
-			g.drawString(field.get(i).getAttack() + "/" + field.get(i).getDefense(), width / 2 - (width / 20) * (field.size()) + width / 10 * i+60, 125+240);
+			g.drawString(field.get(i).getAttack() + "/" + field.get(i).getDefense(),
+					width / 2 - (width / 20) * (field.size()) + width / 10 * i + 60, 125 + 240);
 		}
 	}
 
@@ -37,28 +35,7 @@ public class Enemy {
 		return false;
 	}
 
-	public void attack(Player p, Minion e) {
-		for (Minion m : field) {
-			if (p.getField().size() == 0) {
-				p.changeLife(e.getAttack() * -1);
-			} else {
-				Minion target = p.getRandomMinion();
-				m.fight(target);
-				if (m.getDefense() <= 0)
-					destroyCard(m);
-				if (target.getDefense() <= 0)
-					destroyCard(target);
-			}
-		}
-
-	}
-
 	public static ArrayList<Minion> getField() {
 		return field;
 	}
-
-	public void destroyCard(Card c) {
-		// Code should remove c from the field and move it to the graveyard
-	}
-
 }
