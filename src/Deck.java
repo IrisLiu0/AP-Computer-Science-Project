cc36b939660445204b77487ff35275ac6071d695
Iris <>
2018-06-18 22:29:51 -0400

    changed deck

--- src/Deck.java
+++ src/Deck.java
@@ -8,12 +8,14 @@ public class Deck {
 
 	public Deck() {
 		myDeck = new ArrayList<Card>();
-		myDeck.addAll(Arrays.asList(new Spell("Weaken", 1, 0), new Minion("Ego-bloated Blob", 1, 1, 1, 1),
+		myDeck.addAll(Arrays.asList(new Spell("Weaken", 1, 0), new Spell("Weaken", 1, 0),
 				new Minion("Ego-bloated Blob", 1, 1, 1, 1), new Minion("Ego-bloated Blob", 1, 1, 1, 1),
-				new Minion("Supportive Blob", 2, 2, 2, 2), new Minion("Cruel Blob", 2, 3, 2, 2),
-				new Spell("Drain Life", 3, 4), new Spell("Deadly Shot", 3, 5), new Minion("Healthy Blob", 3, 6, 3, 3),
+				new Minion("Ego-bloated Blob", 1, 1, 1, 1), new Minion("Supportive Blob", 2, 2, 2, 2),
+				new Minion("Cruel Blob", 2, 3, 2, 2), new Minion("Cruel Blob", 2, 3, 2, 2),
+				new Spell("Drain Life", 3, 4), new Spell("Drain Life", 3, 4), new Spell("Deadly Shot", 3, 5),
+				new Spell("Deadly Shot", 3, 5), new Minion("Healthy Blob", 3, 6, 3, 3),
+				new Minion("Healthy Blob", 3, 6, 3, 3), new Minion("Sneaky Blob", 3, 7, 4, 2),
 				new Minion("Sneaky Blob", 3, 7, 4, 2), new Minion("Sneaky Blob", 3, 7, 4, 2),
-				new Minion("Sneaky Blob", 3, 7, 4, 2), new Spell("Panem et Circenses", 3, 8),
 				new Minion("Absolute Beefcake", 4, 9, 2, 7), new Minion("Absolute Beefcake", 4, 9, 2, 7),
 				new Minion("Absolute Beefcake", 4, 9, 2, 7)));
 	}
